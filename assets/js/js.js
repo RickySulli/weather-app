a9841843d65c87297abc53957b76abaa44d46237
Ricky Sullivan <rickyplaysdrums@gmail.com>
2020-10-30 22:33:33 -0500

    bones

---
+++ assets/js/js.js
@@ -0,0 +1,6 @@
+//add a function to get city info
+var getCity = function(){
+    fetch("api.openweathermap.org/data/2.5/forecast?q={austin}&appid={a840377086e495c774022c3af8331d79}");
+    
+}
+getCity();
